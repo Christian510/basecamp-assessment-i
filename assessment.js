16f7a9021262fc7ad34a4c86fd0492c3ea7b4a85
Christian's Mac <macbook@Macbooks-MacBook-Pro.local>
2018-10-22 06:10:09 -0600

    Assessment complete.

--- assessment.js
+++ assessment.js
@@ -3,16 +3,14 @@
 let myName = "Christian";
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
-// CODE HERE
+let faveNum = 1972;
 
 // #3 create a variable called 'lovesCode' and set it to true
 
-// CODE HERE
-
+let lovesCode = "true";
 // #4 Using the previously created variables, create a new variable called 'me' that uses the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
 
-// CODE HERE
-
+let me = myName + "'s " + "favorite number is " + faveNum;
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
 
 // CODE HERE
